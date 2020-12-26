35bf932c6e4be0659ad148819b7c66c070aa1440
Gaurav patidar <gp26101998@gmail.com>
2020-12-26 14:35:01 +0530

    first design template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StudioComponent } from './studio/studio.component';
+import { MissionComponent } from './mission/mission.component';
+import { ManagementComponent } from './management/management.component';
+import { FeaturesComponent } from './features/features.component';
+import { TeamComponent } from './team/team.component';
+import { ServiceComponent } from './service/service.component';
+import { CreativeComponent } from './creative/creative.component';
+import { LinksComponent } from './links/links.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StudioComponent,
+    MissionComponent,
+    ManagementComponent,
+    FeaturesComponent,
+    TeamComponent,
+    ServiceComponent,
+    CreativeComponent,
+    LinksComponent
   ],
   imports: [
     BrowserModule,
