f448d9ab77b91dbb9a7061b8424431982f16bd00
Gaurav patidar <gp26101998@gmail.com>
2021-04-23 20:38:24 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
     let path=localStorage.getItem("path");
     if(path){
       localStorage.removeItem('path')
-      console.log(path)
+      console.log(path.split("/")[1])
       this.router.navigate(["../"+path.split("/")[1]])
     }
   }
