a5554ecb7e1616bd3fb5d45f25e8f089b53896f5
Gaurav patidar <gp26101998@gmail.com>
2021-04-28 21:58:09 +0530

    reverting commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import {filter} from 'rxjs/operators';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,11 +7,8 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'design';
-  route;
   constructor(private router:Router){
-this.router.events.pipe(filter(event=>
-event instanceof NavigationEnd)).subscribe((data:any)=>
-  this.route=data.url)
+ 
   }
   ngOnInit(): void {
    
