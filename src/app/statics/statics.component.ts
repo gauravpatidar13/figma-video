dda0eeb5787190d0467260829e91222b44f2ffd1
Gaurav patidar <gp26101998@gmail.com>
2021-05-02 17:06:30 +0530

    design improved

--- src/app/statics/statics.component.ts
+++ src/app/statics/statics.component.ts
@@ -6,7 +6,7 @@ import {
   ApexXAxis,
   ApexTitleSubtitle
 } from "ng-apexcharts";
-import {} from 'apexcharts';
+import { } from 'apexcharts';
 export type ChartOptions = {
   series: ApexAxisChartSeries;
   chart: ApexChart;
@@ -19,41 +19,42 @@ export type ChartOptions = {
   styleUrls: ['./statics.component.css']
 })
 export class StaticsComponent implements OnInit {
-title="Apex Chart";
+  title = "Apex Chart";
 
-@ViewChild("chart") chart:ChartComponent;
-public chartOptions;
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions;
   constructor() { }
 
   ngOnInit(): void {
-    this.chartOptions=this.chartOptions = {
-        show:false,
-        points:{
-          marker:{
-            x:"X",
-            y:"y",
-            size:8
-          }
-          ,label:{
-            borderColor:"#f00",
-            text:"point"
-          }
-        },
-        tooltip:{
-        custom:[function({series,seriesIndex,dataPointIndex,w}){
-        
-          return `<div class="box" style="color:#000;padding:5px 8px">${series[seriesIndex][dataPointIndex]}</div>`;
-            },function({series,seriesIndex,dataPointIndex,w}){
-             
-              return `<div class="arrow_box" style="color:#000;padding:5px 8px">${series[seriesIndex][dataPointIndex]}</div>`;
-                }]}
-                ,
-      stroke:{
-        show:true,
-        curver:'smooth',
-        width:2
+    this.chartOptions = this.chartOptions = {
+      show: false,
+      points: {
+        marker: {
+          x: "X",
+          y: "y",
+          size: 8
+        }
+        , label: {
+          borderColor: "#f00",
+          text: "point"
+        }
       },
-      colors:["#173cff","#bebebe"],
+      tooltip: {
+        custom: [function ({ series, seriesIndex, dataPointIndex, w }) {
+
+          return `<div class="box" style="color:#000;padding:5px 8px">${series[seriesIndex][dataPointIndex]}</div>`;
+        }, function ({ series, seriesIndex, dataPointIndex, w }) {
+
+          return `<div class="arrow_box" style="color:#000;padding:5px 8px">${series[seriesIndex][dataPointIndex]}</div>`;
+        }]
+      }
+      ,
+      stroke: {
+        show: true,
+        curver: 'smooth',
+        width: 2
+      },
+      colors: ["#173cff", "#bebebe"],
       series: [
         {
           name: "",
@@ -65,66 +66,70 @@ public chartOptions;
         }
       ],
       chart: {
-        toolbar:{show:false},
+        toolbar: { show: false },
         height: 350,
         type: "area",
-        
+
       },
       title: {
         text: ""
       },
       xaxis: {
-        tooltip:{
-enabled:false
+        tooltip: {
+          enabled: false
         },
-        categories: ["0", "1",  "2",  "3",  "4",  "5",  "6",  "7", "8"],
-     labels:{
-       show:true
-     }
+        categories: ["0", "1", "2", "3", "4", "5", "6", "7", "8"],
+        labels: {
+          show: true
+        }
       },
-      yaxis:{
-tooltip:{
-  enabled:false,
-},
-labels:{
-  show:false
-}
-      },
-      grid:{
-        position:"front",
+      yaxis: {
+        tooltip: {
+          enabled: false,
+        },
+        labels: {
+          show: true
+        },
         show:true,
-        xaxis:{
-          lines:{
-            show:false
+        opposite:true,
+        showAlways:true,
+       
+      },
+      grid: {
+        position: "front",
+        show: true,
+        xaxis: {
+          lines: {
+            show: false
           }
         },
-        yaxis:{
-          lines:{
-            show:true
+        yaxis: {
+          lines: {
+            show: true
           }
         }
       },
-      markers:{
-        size:[0,0],
-        enabled:false,
-        show:false,
-        shape:"circle",
-        discrete:[],
+      markers: {
+        size: [0, 0],
+        enabled: false,
+        show: false,
+        shape: "circle",
+        discrete: [],
       }
       ,
-      toolbar:{
-        show:false
+      toolbar: {
+        show: false
       },
-      dataLabels:{
-        enabled:false
+      dataLabels: {
+        enabled: false
       }
-     ,legend:{
-       show:false
-     }
+      , legend: {
+        show: false
+      }
     };
 
     var chart = new ApexCharts(document.querySelector("#chart"), this.chartOptions);
-chart.render();
+    chart.render();
   }
 
 }
